79d146147d71ce6b0d85edc2a6b61439362b5c02
cjhong <cjhong6@gmail.com>
2020-03-08 18:40:26 -0700

    boucing ball

--- pinball.py
+++ pinball.py
@@ -5,14 +5,14 @@ import random
 import time
 import tkinter # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
 
-
 # Communicate with the window manager
-tk = tkinter.Tk()
+tk = tkinter.Tk() #create TK window
 tk.title("Pinball") #Set window title
 tk.resizable(0,0) #The width flag controls whether the window can be resized horizontally by the user.(0,0) means can't be resize
 tk.wm_attributes("-topmost", 1) #this window is always placed on top of other windows
 canvas = tkinter.Canvas(tk, width=500, height=400, bd=0, highlightthickness=0, bg="powderblue") #create a canvas, bd=borderwidth
-canvas.pack() #organizes widgets in blocks before placing them in the parent widget
+canvas.pack() #put the canvas(widget) to the tk window
+tk.update() #Update the window size
 
 #create pinball object
 class Ball():
@@ -20,13 +20,21 @@ class Ball():
         self.canvas = canvas
         self.id=canvas.create_oval(10,10,25,25,fill=color)
         self.canvas.move(self.id,x,y)
+        self.xSpeed = 0
+        self.ySpeed = 1
+        self.canvas_height = self.canvas.winfo_height()
+
     def draw(self):
-        self.canvas.move(self.id,0,-1)
+        self.canvas.move(self.id,self.xSpeed,self.ySpeed)
+        pos = self.canvas.coords(self.id)
+        if(pos[1]<=0):
+            self.ySpeed = 1
+        if(pos[3]>=self.canvas_height):
+            self.ySpeed = -1
 
 ball = Ball(250,200,canvas, '#ff546e')
 
 while True:
     ball.draw()
-    tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
